c5476d58e1cbfa712491454235152c33e1a5c56f
HongQiLi <hongqi.li@colorado.edu>
2025-06-03 10:37:00 -0600

    Create Footer.tsx

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="mt-24 py-8 w-full text-center text-sm text-neutral-500 border-t">
+      <p>&copy; {new Date().getFullYear()} Reddie Taggie. All rights reserved.</p>
+    </footer>
+  );
+}
