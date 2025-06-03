f55018e5ba646462d1b0cff15f600a2cb9911fa7
HongQiLi <hongqi.li@colorado.edu>
2025-06-03 11:35:09 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Hero from "@/components/Hero";
-import Footer from "@/components/Footer";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
