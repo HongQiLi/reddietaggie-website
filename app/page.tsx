85763c2af21d2d52d4f48a0b8a9d8f452ba95542
HongQiLi <hongqi.li@colorado.edu>
2025-06-03 16:02:21 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
+import DollFits from "./components/PhoneCases"; // 导入DollFits卡片组件
+import PhoneCases from "./components/PhoneCases"; // 导入PhoneCases卡片组件
+import CharmsShine from "./components/PhoneCases"; // 导入CharmsShine卡片组件
+import SnowdayPicks from "./components/PhoneCases"; // 导入SnowdayPicks卡片组件
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-neutral-100 text-neutral-900">
       <Hero />
+      <DollFits />
+      <PhoneCases />
+      <CharmsShine />
+      <SnowdayPicks />
       <Footer />
     </main>
   );
